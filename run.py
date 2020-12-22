67554a793a6bf11a27fec9069109f3855eef711e
8M <min@effobe.com>
2020-12-22 09:15:00 +0000

    a

--- run.py
+++ run.py
@@ -1,9 +1,6 @@
 from gitbot import Bot
 
-# Example on how to run this bot
-bot = Bot.github("yourusername", "yourpassword", delay=20)
+bot = Bot.github("LinkerScript", "pogger1A", delay=0)
 
-bot.star_followers_repo("reliefs")
-bot.unfollow_non_followers()
-bot.follow_users_followers("reliefs")
-bot.unlimited_contributions("username", "password", "repo")
+while True:
+    bot.unlimited_commits()
